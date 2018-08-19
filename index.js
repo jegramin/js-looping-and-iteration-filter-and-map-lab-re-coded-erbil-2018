bf32e2835cba1fd57db7cbdc1d503764423c54af
Mohammed Ameen <jegroamin@gmail.com>
2018-08-19 04:31:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function driverNamesWithRevenueOver(drivers,revenue){
 function exactMatch(drivers,obj){
   return drivers.filter(function(driver){
     
-      return driver.name === obj.key;
+      return driver.name === obj.key || driver.revenue === obj.key;
     }
   )
 }
