4e720b5e964f2cf5009a10d0ddbebf58a6750e73
Mohammed Ameen <jegroamin@gmail.com>
2018-08-19 04:06:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers,revenue){
+  return drivers.filter(function(name){
+    if(name.revenue > revenue){
+      return {name: name.name, revenue: name.revenue};
+    }
+  })
+}
+
+function driverNamesWithRevenueOver(drivers,revenue){
+  return driversWithRevenueOver(drivers,revenue).map(function(name){
+    return name.name;
+  })
+}
